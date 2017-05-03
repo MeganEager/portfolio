6d2cfc8e8652c2d5db7ed83d2b66591d6f0cbc7b
Megan Eager <meganeager1@gmail.com>
2017-05-03 19:38:52 +0100

    Changes and images

--- js/grid.js
+++ js/grid.js
@@ -345,11 +345,12 @@ var Grid = (function() {
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
 			this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
+			this.$closePreview = $( '<span class="og-close"></span>' );
+			this.$details = $( '<div class="og-details"></div>' ).append( this.$closePreview, this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
-			this.$closePreview = $( '<span class="og-close"></span>' );
-			this.$previewInner = $( '<div class="og-expander-inner"></div>' ).append( this.$closePreview, this.$fullimage, this.$details );
+
+			this.$previewInner = $( '<div class="og-expander-inner"></div>' ).append(this.$fullimage, this.$details );
 			this.$previewEl = $( '<div class="og-expander"></div>' ).append( this.$previewInner );
 			// append preview element to the item
 			this.$item.append( this.getEl() );
